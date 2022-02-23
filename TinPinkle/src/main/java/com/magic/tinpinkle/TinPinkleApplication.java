45958a06b91b77699a4c026743ae6d250e8bcb8b
Jassie <jasminehart94@gmail.com>
2022-02-23 10:12:45 -0800

    created spring file

---
+++ TinPinkle/src/main/java/com/magic/tinpinkle/TinPinkleApplication.java
@@ -0,0 +1,13 @@
+package com.magic.tinpinkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinPinkleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinPinkleApplication.class, args);
+    }
+
+}
