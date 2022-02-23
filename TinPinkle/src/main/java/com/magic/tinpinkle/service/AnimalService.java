28320a35501d123bdb8ed031354b78bb5ad2f9ca
Jassie <jasminehart94@gmail.com>
2022-02-23 13:48:01 -0800

    Created the Animal Service

--- TinPinkle/src/main/java/com/magic/tinpinkle/service/AnimalService.java
+++ TinPinkle/src/main/java/com/magic/tinpinkle/service/AnimalService.java
@@ -1,4 +1,27 @@
 package com.magic.tinpinkle.service;
 
+
+import com.magic.tinpinkle.model.Animal;
+import com.magic.tinpinkle.repository.AnimalRepository;
+import jdk.dynalink.linker.LinkerServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class AnimalService {
+
+    private AnimalRepository animalRepository;
+
+    @Autowired
+    public void setAnimalRepository(AnimalRepository animalRepository){
+        this.animalRepository = animalRepository;
+    }
+
+    public List<Animal> getAllAnimals() {
+        return animalRepository.findAll();
+    }
+
+
 }
