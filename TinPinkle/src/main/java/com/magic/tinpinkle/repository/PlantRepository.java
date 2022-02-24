5dd3c9388a1c6b71e7ca834243f7feb6db983dba
Jassie <jasminehart94@gmail.com>
2022-02-24 08:21:00 -0800

    Added to the Plant Service

--- TinPinkle/src/main/java/com/magic/tinpinkle/repository/PlantRepository.java
+++ TinPinkle/src/main/java/com/magic/tinpinkle/repository/PlantRepository.java
@@ -4,4 +4,6 @@ import com.magic.tinpinkle.model.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlantRepository extends JpaRepository<Plant, Long> {
+
+    Plant findByName(String name);
 }
--- TinPinkle/src/main/java/com/magic/tinpinkle/service/PlantService.java
+++ TinPinkle/src/main/java/com/magic/tinpinkle/service/PlantService.java
@@ -1,12 +1,15 @@
 package com.magic.tinpinkle.service;
 
 
+import com.magic.tinpinkle.model.Animal;
 import com.magic.tinpinkle.model.Plant;
+import com.magic.tinpinkle.repository.AnimalRepository;
 import com.magic.tinpinkle.repository.PlantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlantService {
@@ -22,4 +25,36 @@ public class PlantService {
         return plantRepository.findAll();
     }
 
+
+    public Plant createPlant(Plant plantObject) {
+
+        Plant create = plantRepository.findByName(plantObject.getName());
+
+        return plantRepository.save(create);
+    }
+
+    public Optional<Plant> getPlants(Long plantId) {
+        Optional<Plant> plant = plantRepository.findById(plantId);
+
+        return plant;
+    }
+
+    public Plant updatePlants(Long plantId, Plant plantObject) {
+
+        Plant plant = plantRepository.findById(plantId).get();
+
+        plant.setName(plantObject.getName());
+        plant.setDescription(plantObject.getDescription());
+        plant.setFlower(plantObject.getFlower());
+        plant.setRoot(plantObject.getRoot());
+        plant.setStem(plantObject.getStem());
+        return plantRepository.save(plant);
+    }
+
+    public Plant deletePlant(Long plantId) {
+        Plant plant = plantRepository.findById(plantId).get();
+
+        return plant;
+
+    }
 }
